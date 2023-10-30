deb67e044e8a54cf7a31a87d9844bc369959f379
joshpickardme <hello@joshpickard.me>
2023-10-30 12:52:32 +0000

    entry task complete

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+
+async function getImage(query) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+    const data = await response.json()
+    return data.data[0].url
+}
+
+getImage("hello").then(console.log)
