9cd82064aa32d099255d54e45a17d7ba688d5fe6
joshpickardme <hello@joshpickard.me>
2023-10-30 13:07:59 +0000

    getFirstImage() getRandomImage() getAllImages()

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ console.log(process.env.API_KEY)
 async function getImage(query) {
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
     const data = await response.json()
-    return data.data[0].url
+    return data.data
 }
 
-getImage("hello").then(console.log)
+async function getFirstImage(query) {
+    const data = await getImage(query);
+    return data[0].url
+}
+
+async function getRandomImage(query) {
+    const data = await getImage(query)
+    const randomNum = Math.floor(Math.random() * data.length);
+    return data[randomNum].url
+}
+
+async function getAllImages(query) {
+    const data = await getImage(query)
+    const arr = []
+    for(let i = 0; i < data.length; i++) {
+        arr.push(data[i].url)
+    }
+    return arr
+}
+
+
+getAllImages("blue").then(console.log)
+
